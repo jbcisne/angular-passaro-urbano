aea5ca2d6deb2ac9c51ad25e74d6ac8aa9f2c25e
Juliano Buzanello <jbcisne@gmail.com>
2018-08-21 21:24:44 -0300

    Incluindo diretiva ngSubmit para submeter os dados do form para a classe

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { OrdemCompraService } from '../ordem-compra.service'
 import { Pedido } from '../shared/pedido.model'
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-ordem-compra',
@@ -15,5 +16,10 @@ export class OrdemCompraComponent implements OnInit {
   ngOnInit() {
     
   }
+
+  public confirmarCompra(form: NgForm): void {
+    console.log(form)
+  }
+
 }
  
