0b7e48612bb2e6ba5a95683b8d6ba1b908cde41b
Juliano Buzanello <jbcisne@gmail.com>
2018-08-29 20:46:46 -0300

    Implementação da submissão do formulario para salvar os dados

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { OrdemCompraService } from '../ordem-compra.service'
-//import { Pedido } from '../shared/pedido.model'
 import { NgForm } from '@angular/forms';
+import { Pedido } from '../shared/pedido.mode';
 
 @Component({
   selector: 'app-ordem-compra',
@@ -13,6 +13,8 @@ export class OrdemCompraComponent implements OnInit {
 
   @ViewChild('formulario') public form: NgForm
 
+  public idPedidoCompra: number
+
   constructor(private ordemCompraService: OrdemCompraService) { }
 
   ngOnInit() {
@@ -20,7 +22,17 @@ export class OrdemCompraComponent implements OnInit {
   }
 
   public confirmarCompra(): void {
-    console.log(this.form)
+    let pedido: Pedido = new Pedido(
+      this.form.value.endereco,
+      this.form.value.numero,
+      this.form.value.complemento,
+      this.form.value.formaPagamento,
+    )
+
+    this.ordemCompraService.efetivarCompra(pedido).subscribe(
+      (idPedido: number) => {
+        this.idPedidoCompra = idPedido
+      })
   }
 
 }
